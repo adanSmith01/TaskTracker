3b7a131c6708871902eccb278509df43d71bc317
adanSmith01 <smith.paredes.13@gmail.com>
2025-01-31 01:38:19 -0300

    feat: add main program of the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("task-cli ");
+        Console.ForegroundColor = ConsoleColor.White;
+        string command = Console.ReadLine();
+        Console.WriteLine("In progress");
+    }
+
+}
