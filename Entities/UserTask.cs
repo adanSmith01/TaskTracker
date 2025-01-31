1e75ccc6e55b4ca548f769d321f12bd7574e135d
adanSmith01 <smith.paredes.13@gmail.com>
2025-01-31 13:57:48 -0300

    feat: add 'UserTask' entity

---
+++ Entities/UserTask.cs
@@ -0,0 +1,26 @@
+namespace TaskTracker.Entities;
+
+public enum State
+{
+    ToDo,
+    InProgress,
+    Done
+}
+
+/// <summary>
+/// This name was chosen to avoid confunsion (and problems) 
+/// with the 'Task' class related to 'Threads'
+/// </summary>
+
+public class UserTask
+{
+    public int Id { get; set;}
+
+    public string Description { get; set;}
+
+    public State state{ get; set;}
+
+    public DateTime CreatedAt { get; set;}
+    
+    public DateTime UpdatedAt { get; set;}
+}
