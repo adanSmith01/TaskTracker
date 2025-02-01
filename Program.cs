4e976e5614ee3f07c87301787f613826ef211031
adanSmith01 <smith.paredes.13@gmail.com>
2025-01-31 21:21:03 -0300

    feat: add task management methods and refactor existing code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,63 @@
-﻿internal class Program
+﻿using System.Text.RegularExpressions;
+using TaskTracker.Entities;
+using TaskTracker.Services;
+
+internal class Program
 {
+    const string SPLIT_PATTERN = @"(\"".*?\"")|\S+";
+    const string CREATE_OPERATION = "create";
+    const string CREATE_PATTERN = @"^create\s""(.*)""\s--state\s[0-2]$";
+
     private static void Main(string[] args)
+    {
+        try
+        {
+            string command = string.Empty;
+
+            do
+            {
+                printInitialLine();
+                IUserTaskService _userTaskService = new UserTaskService("userTasks.json");
+                command = Console.ReadLine();
+                executeComand(command, _userTaskService);
+            }while(!commandIsValid(command));
+
+            Console.WriteLine("In Progress...");
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private static void printInitialLine()
     {
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.Write("task-cli ");
         Console.ForegroundColor = ConsoleColor.White;
-        string command = Console.ReadLine();
-        Console.WriteLine("In progress");
     }
 
+    private static bool commandIsValid(string command)
+    {
+        string[] validSyntaxCommands = {CREATE_PATTERN};
+        foreach (var validSyntaxCommand in validSyntaxCommands)
+        {
+            if(Regex.IsMatch(command, validSyntaxCommand)) return true;
+        }
+
+        return false;
+    }
+
+    private static void executeComand(string command, IUserTaskService userTaskService)
+    {
+        var splitCommand = Regex.Matches(command, SPLIT_PATTERN);
+        string operation = splitCommand[0].Value;
+
+        if(operation == CREATE_OPERATION)
+        {
+            userTaskService.Create(splitCommand[1].Value.Trim('"'), (State)Convert.ToInt32(splitCommand[3].Value.Trim('"')));
+            Console.WriteLine("Task created.\n");
+        }
+        
+    }
 }
